d62cea91384a5b8d495be3ebdde7160ac23fb619
NerdBook <kyleslux86@gmail.com>
2021-10-13 11:59:59 -0400

    fixed static directory location

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static(public));
+app.use(express.static('./public'));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
