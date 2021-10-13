2079f49b9b8c3c1da939178427d882ca937477c8
Kyle Lux <kyleslux86@gmail.com>
2021-10-13 11:04:44 -0400

    Update server.js
    forgot to include uuid requirement in previous commit.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const db = require('./db/db.json');
+const uuid = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
