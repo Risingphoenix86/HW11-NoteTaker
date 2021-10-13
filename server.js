a72a13764ec45e7ae5d333588c5efdcb309e55c0
Kyle Lux <kyleslux86@gmail.com>
2021-10-13 11:13:08 -0400

    Update server.js
    added missing ;s

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(express.json());
 app.use(express.static(public));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'));
 );
     
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 );
 
 app.get('/api/notes', (req, res) => {
@@ -24,11 +24,11 @@ app.get('/api/notes', (req, res) => {
 });
     
 app.post('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
     const newNotes = req.body;
     newNotes.id = uuid.v4();
     notes.push(newNotes);
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
     res.json(notes);
 })
 
